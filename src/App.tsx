3f6a81d58ba98a31aa5e8720b0bf05cf26f68de0
Fernando <colom_fernando@bitbucket.org>
2022-03-19 11:12:06 -0300

    setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GlobalStyles from 'theme/GlobalStyles';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyles />
+    </>
+  );
+};
+
+export default App;
